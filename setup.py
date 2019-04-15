a0c2b852bbb2c40015dfa5356f30951bc18a01ce
Parantapa Bhattacharya <pb@parantapa.net>
2019-04-15 12:49:22 -0400

    Update setup.py to use stuff from README.md

--- setup.py
+++ setup.py
@@ -3,9 +3,20 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+    lines = long_description.strip()
+    lines = lines.split("\n")
+    lines = [line.strip() for line in lines]
+    lines = [line for line in lines if line]
+
+    package_name = lines[0].lstrip("#").strip()
+    print("package_name:", package_name)
+
+    description = lines[1]
+    print("description:", description)
+
 setup(
-    name="qz7-shell",
-    description="Utility functions to run code on local and remote systems.",
+    name=package_name,
+    description=description,
 
     author="Parantapa Bhattacharya",
     author_email="pb+pypi@parantapa.net",
@@ -13,8 +24,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
 
-    packages=["qz7.shell"],
-    scripts=["bin/screenlog"],
+    packages=[package_name],
 
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
@@ -24,7 +34,7 @@ setup(
         "paramiko"
     ],
 
-    url="http://github.com/parantapa/qz7-shell",
+    url="http://github.com/parantapa/%s" % package_name,
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
