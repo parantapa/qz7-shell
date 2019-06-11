cf4c2307427891a9c0581c0372840489b8e3cbab
Parantapa Bhattacharya <pb@parantapa.net>
2019-06-11 13:26:30 -0400

    Allow command_format to be imported as command

--- qz7/shell/__init__.py
+++ qz7/shell/__init__.py
@@ -3,4 +3,8 @@ Helper functions to execute shell commands locally and via ssh.
 """
 
 from qz7.shell.cmdlist import command_format
+
+# NOTE: Use of command instead of command_format is deprecated
+command = command_format
+
 # from qz7.shell.run import remote, remote_m, local, set_term
